d5a869d14f2ff6aec184636edf115a9be62a1a1c
Mohammed Anas FIKHI <work@gwhyyy.com>
2023-10-04 00:21:16 +0100

    [ Add ] added mofified play template

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,9 @@
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/logo-2.svg";
+      logo.src = "./assets/images/brands/colotrd_bg.png";
     } else {
-      logo.src = "assets/images/logo/logo.svg";
+      logo.src = "./assets/images/brands/full_logo_white.png";
     }
 
     // show or hide the back-top-top button
